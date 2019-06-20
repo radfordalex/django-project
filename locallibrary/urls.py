1b23b0f3501395fe9368542af1a42c72248c5f1a
alex <radfordalex46@yahoo.com>
2019-06-20 16:58:49 -0400

    chapter 2-3

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -34,6 +34,20 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
+# Use include() to add paths from the catalog application 
+from django.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
